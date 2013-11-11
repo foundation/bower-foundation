81cc45d9675a1ca5b3bf19b3f42758478e9be44d
mhayes <mark@zurb.com>
2013-11-11 18:31:51 +0000

    Foundation build to bower-foundation

--- js/foundation.js
+++ js/foundation.js
@@ -363,7 +363,8 @@ if (typeof jQuery === "undefined" &&
       form
         .off('.abide')
         .on('submit.fndtn.abide validate.fndtn.abide', function (e) {
-          return self.validate($(this).find('input, textarea, select').get(), e);
+          var is_ajax = /ajax/i.test($(this).attr('data-abide'));
+          return self.validate($(this).find('input, textarea, select').get(), e, is_ajax);
         })
         .find('input, textarea, select')
           .off('.abide')
@@ -379,13 +380,14 @@ if (typeof jQuery === "undefined" &&
           });
     },
 
-    validate : function (els, e) {
+    validate : function (els, e, is_ajax) {
       var validations = this.parse_patterns(els),
           validation_count = validations.length,
-          form = $(els[0]).closest('form');
+          form = $(els[0]).closest('form'),
+          submit_event = /submit/.test(e.type);
 
       for (var i=0; i < validation_count; i++) {
-        if (!validations[i] && /submit/.test(e.type)) {
+        if (!validations[i] && (submit_event || is_ajax)) {
           if (this.settings.focus_on_invalid) els[i].focus();
           form.trigger('invalid');
           $(els[i]).closest('form').attr('data-invalid', '');
@@ -393,11 +395,13 @@ if (typeof jQuery === "undefined" &&
         }
       }
 
-      if (/submit/.test(e.type)) {
+      if (submit_event || is_ajax) {
         form.trigger('valid');
       }
 
       form.removeAttr('data-invalid');
+
+      if (is_ajax) return false;
 
       return true;
     },
