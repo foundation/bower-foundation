861c98c57b92759616e99bf436fa25f82251d094
mhayes <mark@zurb.com>
2013-11-16 04:08:47 +0000

    Foundation build to bower-foundation

--- js/foundation.js
+++ js/foundation.js
@@ -165,9 +165,9 @@
     version : '5.0.0',
 
     media_queries : {
-      small : S('.foundation-mq-small').css('font-family').replace(/^[\\'"]+|(;\s?})+|[\\'"]+$/g, ''),
-      medium : S('.foundation-mq-medium').css('font-family').replace(/^[\\'"]+|(;\s?})+|[\\'"]+$/g, ''),
-      large : S('.foundation-mq-large').css('font-family').replace(/^[\\'"]+|(;\s?})+|[\\'"]+$/g, '')
+      small : S('.foundation-mq-small').css('font-family').replace(/\'/g, ''),
+      medium : S('.foundation-mq-medium').css('font-family').replace(/\'/g, ''),
+      large : S('.foundation-mq-large').css('font-family').replace(/\'/g, '')
     },
 
     stylesheet : $('<style></style>').appendTo('head')[0].sheet,
@@ -397,7 +397,6 @@
   };
 
 }(jQuery, this, this.document));
-
 ;(function ($, window, document, undefined) {
   'use strict';
 
