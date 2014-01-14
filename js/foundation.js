3273f4dbad284ad3dcc55c5afc2e132b053efe0a
mhayes <mark@zurb.com>
2014-01-14 23:30:55 +0000

    Foundation build to bower-foundation

--- js/foundation.js
+++ js/foundation.js
@@ -3595,7 +3595,7 @@
   Foundation.libs.tooltip = {
     name : 'tooltip',
 
-    version : '5.0.0',
+    version : '5.0.3',
 
     settings : {
       additional_inheritable_classes : [],
@@ -3714,7 +3714,7 @@
           'top' : (top) ? top : 'auto',
           'bottom' : (bottom) ? bottom : 'auto',
           'left' : (left) ? left : 'auto',
-          'right' : (right) ? right : 'auto',
+          'right' : (right) ? right : 'auto'
         }).end();
       };
 
