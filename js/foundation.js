7c6f8d92195090fbeaeedf07ab3cc3f94a1d61da
mhayes <mark@zurb.com>
2014-10-28 23:09:47 +0000

    Foundation build to bower-foundation

--- js/foundation.js
+++ js/foundation.js
@@ -4287,7 +4287,8 @@
           $.extend(ajax_settings, {
             success: function (data, textStatus, jqXHR) {
               if ( $.isFunction(old_success) ) {
-                old_success(data, textStatus, jqXHR);
+                var result = old_success(data, textStatus, jqXHR);
+                if (typeof result == 'string') data = result;
               }
 
               modal.html(data);
