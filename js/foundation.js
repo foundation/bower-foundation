fbbca20527c6c3c54649dd33887586cd22d63033
mhayes <mark@zurb.com>
2014-11-13 19:12:52 +0000

    Foundation build to bower-foundation

--- js/foundation.js
+++ js/foundation.js
@@ -1633,6 +1633,7 @@
     init : function (scope, method, options) {
       Foundation.inherit(this, 'throttle');
 
+      $.extend(true, this.settings, method, options);
       this.bindings(method, options);
     },
 
