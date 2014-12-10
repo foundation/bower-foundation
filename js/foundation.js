3d0476d2d89b6dabb8a48085b80c77f796e5cccf
mhayes <mark@zurb.com>
2014-12-10 18:28:52 +0000

    Foundation build to bower-foundation

--- js/foundation.js
+++ js/foundation.js
@@ -1758,6 +1758,11 @@
         })
         .on('click.fndtn.dropdown', function (e) {
           var parent = S(e.target).closest('[' + self.attr_name() + '-content]');
+          var links  = parent.find('a');
+
+          if (links.length > 0 && parent.attr('aria-autoclose') !== "false") {
+              self.close.call(self, S('[' + self.attr_name() + '-content]'));
+          }
 
           if (S(e.target).closest('[' + self.attr_name() + ']').length > 0) {
             return;
