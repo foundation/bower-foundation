3b67c37b93b55c08a30250818c96edb2ddb13649
mhayes <mark@zurb.com>
2015-01-08 20:03:50 +0000

    Foundation build to bower-foundation

--- js/foundation.js
+++ js/foundation.js
@@ -4461,9 +4461,6 @@
         }
 
         this.key_up_on(modal);    // PATCH #3: turning on key up capture only when a reveal window is open
-        // modal.on('open.fndtn.reveal').trigger('open.fndtn.reveal', function(e) {
-        //   console.log(e);
-        // });
 
         modal.on('open.fndtn.reveal').trigger('open.fndtn.reveal');
 
