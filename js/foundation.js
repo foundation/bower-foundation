b76942898c31fda9c57bdba530c345b797854cf2
arthurzurb <arthur@zurb.com>
2015-04-24 23:53:23 +0000

    Foundation build to bower-foundation

--- js/foundation.js
+++ js/foundation.js
@@ -5543,8 +5543,8 @@
       target.siblings().removeClass(settings.active_class).attr({'aria-hidden' : 'true',  tabindex : -1});
       target.addClass(settings.active_class).attr('aria-hidden', 'false').removeAttr('tabindex');
       settings.callback(tab);
-      target.triggerHandler('toggled', [tab]);
-      tabs.triggerHandler('toggled', [target]);
+      target.triggerHandler('toggled', [target]);
+      tabs.triggerHandler('toggled', [tab]);
 
       tab_link.off('keydown').on('keydown', interpret_keyup_action );
     },
