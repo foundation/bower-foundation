bcce5ed3df7e78584fb73ed678ebda26e8262cef
arthurzurb <arthur@zurb.com>
2015-09-01 22:06:13 +0000

    Foundation build to bower-foundation

--- js/foundation.js
+++ js/foundation.js
@@ -3787,11 +3787,10 @@
               'scrollTop' : scroll_top
             }, settings.duration, settings.easing, function () {
               if (history.pushState) {
-                        history.pushState(null, null, anchor.pathname + '#' + hash);
+                history.pushState(null, null, anchor.pathname + anchor.search + '#' + hash);
+              } else {
+                location.hash = anchor.pathname + anchor.search + '#' + hash;
               }
-                    else {
-                        location.hash = anchor.pathname + '#' + hash;
-                    }
             });
           }
         })
